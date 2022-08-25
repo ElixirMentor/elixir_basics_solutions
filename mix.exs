22484976a86841b235abf8c9b9c18923512794c6
Jacob Luetzow <jacob@perfectform.io>
2022-08-25 08:25:42 -0600

    generated exdocs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule ElixirBasicsSolutions.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Docs
+      name: "Elixir Basics Solutions",
+      source_url: "https://github.com/jacobluetzow/elixir_basics_solutions",
+      homepage_url: "https://linktr.ee/jacob_luetzow",
+      docs: [
+        main: "Elixir Basics Solutions",
+        logo: "assets/images/bs_logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,8 +30,7 @@ defmodule ElixirBasicsSolutions.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
 end
