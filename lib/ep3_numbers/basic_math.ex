5079fa66ad6c66d75ad49efd5c6a2028aecfb63e
Jacob Luetzow <jacob@perfectform.io>
2022-08-24 20:52:33 -0600

    added documentation to basic_math.ex and calculater.ex

--- lib/ep3_numbers/basic_math.ex
+++ lib/ep3_numbers/basic_math.ex
@@ -3,35 +3,184 @@ defmodule BasicMath do
   Documentation for `BasicMath`.
   """
 
-  def add(a, b) do
-    a + b
+  @doc """
+  Returns the sum of two values `addend_1` and `addend_2`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - addend_1: number (integer or float)
+  - addend_2: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.add(4, 5)
+      9
+
+      iex> BasicMath.add(4.6, 8.9)
+      13.5
+
+  """
+  @doc since: "0.1.0"
+  def add(addend_1, addend_2) do
+    addend_1 + addend_2
   end
 
-  def subtract(a, b) do
-    a - b
+  @doc """
+  Returns the difference of two values `minuend` and `subtrahend`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - minuend: number (integer or float)
+  - subtrahend: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.subtract(4, 5)
+      -1
+
+      iex> BasicMath.subtract(33.3, 8.9)
+      24.4
+
+  """
+  @doc since: "0.1.0"
+  def subtract(minuend, subtrahend) do
+    minuend - subtrahend
   end
 
-  def multiply(a, b) do
-    a * b
+  @doc """
+  Returns the product of two values `multiplier` and `multplicand`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - multiplier: number (integer or float)
+  - multplicand: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.multiply(4, 5)
+      20
+
+      iex> BasicMath.mulitply(33.3, 8.9)
+      296.37
+
+  """
+  def multiply(multiplier, multplicand) do
+    multiplier * multplicand
   end
 
-  def divide(a, b) do
-    a / b
-    # div(a, b)
+  @doc """
+  Returns the quotient of two values `dividend` and `divisor`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - dividend: number (integer or float)
+  - divisor: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.divide(4, 5)
+      20
+
+      iex> BasicMath.divide(33.3, 8.9)
+      296.37
+
+  """
+  def divide(dividend, divisor) do
+    dividend / divisor
   end
 
+  @doc """
+  Returns the absolute value of given `value`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - value: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.absolute_value(-5.6)
+      5.6
+
+      iex> BasicMath.absolute_value(68)
+      68
+
+  """
   def absolute_value(value) do
     abs(value)
   end
 
+  @doc """
+  Returns a value rounded up to the nearest 100th place of given `value`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - value: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.round_up(5.66667)
+      5.67
+
+      iex> BasicMath.round_up(-33.333334)
+      -33.33
+
+  """
   def round_up(value) do
     Float.ceil(value, 2)
   end
 
+  @doc """
+  Returns a value rounded down to the nearest 100th place of given `value`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - value: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.round_down(5.66667)
+      5.66
+
+      iex> BasicMath.round_down(-33.333334)
+      -33.34
+
+  """
   def round_down(value) do
     Float.floor(value, 2)
   end
 
+  @doc """
+  Returns integer part of given `value`.
+
+  Returns `number`
+
+  ## Parameters
+
+  - value: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.truncate_value(5.66667)
+      5
+
+      iex> BasicMath.truncate_value(-33.333334)
+      -33
+
+  """
   def truncate_value(value) do
     trunc(value)
   end
--- lib/ep3_numbers/calculater.ex
+++ lib/ep3_numbers/calculater.ex
@@ -4,9 +4,27 @@ defmodule Calculator do
   """
   alias BasicMath, as: Math
 
+  @doc """
+  Returns the `percentage` of given `value` rounded up to the nearest 100th.
+
+  Returns `float`
+
+  ## Parameters
+
+  - percentage: number (integer or float)
+  - value: number (integer or float)
+
+  ## Examples
+
+      iex> BasicMath.find_percentage_of_value(25, 100)
+      25.0
+
+      iex> BasicMath.find_percentage_of_value(23, 456)
+      104.89
+
+  """
   def find_percentage_of_value(percentage, value) do
     # value * (percentage / 100)
-    # 23, 456 -> 104.88000000000001
     percentage
     |> BasicMath.divide(100)
     |> Math.multiply(value)
