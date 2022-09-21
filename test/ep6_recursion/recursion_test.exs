3226ffd30afd2bc838042ddac8582b4f3fd0db06
Jacob Luetzow <jacob@perfectform.io>
2022-09-21 17:19:10 -0600

    added tests to linked_lists_test

--- test/ep6_recursion/recursion_test.exs
+++ test/ep6_recursion/recursion_test.exs
@@ -2,4 +2,10 @@ defmodule RecursionTest do
   use ExUnit.Case
   doctest Recursion
 
+  describe "get_equipment_list/0" do
+    test "returns static equipment list" do
+      assert Recursion.get_equipment_list() == ["Space Helmet", "Space Suit", "Snacks", "Grapling Hook", "Probe"]
+    end
+  end
+
 end
