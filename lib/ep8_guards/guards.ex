9e56c1c21698d5c744accefb05235361afdedf22
Jacob Luetzow <jacob@perfectform.io>
2022-10-03 12:03:55 -0600

    code wrapped up

--- lib/ep8_guards/guards.ex
+++ lib/ep8_guards/guards.ex
@@ -2,6 +2,9 @@ defmodule Guards do
 
   def type(value) when is_integer(value), do: :integer
   def type(value) when is_float(value), do: :float
+  def type(value) when not is_number(value), do: :not_number
+
+  def is_single_digit(value) when is_integer(value) and value < 10, do: true
 
   defguard is_even(value) when is_integer(value) and rem(value, 2) == 0
 
@@ -10,9 +13,9 @@ defmodule Guards do
   def return_even_numbers(value) when is_even(value), do: value
   def return_even_numbers(value) when not is_even(value), do: :not_even
 
-  def is_under_max_load?(load \\ 0, max_load \\ 55)
-  def is_under_max_load?(load, max_load) when load <= max_load, do: true
-  def is_under_max_load?(load, max_load) when load > max_load, do: false
+  def is_under_max_load?(load, max_load \\ 55)
+  def is_under_max_load?(load, max_load) when is_number(load) and load <= max_load, do: true
+  def is_under_max_load?(load, max_load) when not is_number(load) or load > max_load, do: false
 
   #use example in conditonal lesson
    # def integer_to_atom(value) do
